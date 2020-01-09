8ad68e07e2c1bb663f2b694848c07e9a9ff8a1d0
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-01-09 23:24:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,23 +6,45 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  hash = {
+    name: 'Eduardo',
+    age: 29
+  }
   
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hash = {
+    name: 'Grace Hopper',
+    age: 29
+  }
+  hash[:name]
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  hash = {
+    id: 1,
+    name: 'Grace Hopper',
+    age: 29
+  }
+  hash[:id]
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  hash = {
+    name: 'Grace Hopper',
+    age: 29,
+    key: value
+  }
+  hash[key]
 end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
